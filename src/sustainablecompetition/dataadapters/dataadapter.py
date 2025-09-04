8f9dd55fbd25b3234511d0e0d98e225ef308738b
Théo Matricon <theomatricon@gmail.com>
2025-09-04 11:15:59 +0200

    add data adapter interface

---
+++ src/sustainablecompetition/dataadapters/dataadapter.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+
+import polars as pl
+
+
+__all__ = ["DataAdapter"]
+
+
+class DataAdapter(ABC):
+    """
+    Enables to interact with the data interactively without loading everything into memory. 
+    """
+
+    @abstractmethod
+    def get_performances(self, benchmark_id: str, solver_id: Optional[str] = None, hardware_id: Optional[str] = None) -> pl.DataFrame:
+        """Get as a data frame all performances for the specific benchmark_id.
+
+
+        Args:
+            benchmark_id (str): the id of the instance to get the performances about
+            solver_id (Optional[str], optional): If set, only gives the performance with the specified id. Defaults to None.
+            hardware_id (Optional[str], optional): If set, only gives the performance with the specified id. Defaults to None.
+
+        Returns:
+            pl.DataFrame: _description_
+        """
+        pass
