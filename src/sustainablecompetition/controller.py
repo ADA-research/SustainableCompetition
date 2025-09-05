8164c1e25937fba210657b9d63f495f3d5900478
Théo Matricon <theomatricon@gmail.com>
2025-09-05 11:02:23 +0200

    async run

--- src/sustainablecompetition/controller.py
+++ src/sustainablecompetition/controller.py
@@ -1,7 +1,10 @@
-from sustainablecompetition.benchmarkatoms import Result
+import asyncio
 from sustainablecompetition.benchmarkingmethods.benchmarkerinterface import Benchmarker
 from sustainablecompetition.infrastructureadapters.abstractrunner import AbstractRunner
 from sustainablecompetition.resultconsumers.resultconsumer import ResultConsumer
+
+
+__all__ = ["Controller"]
 
 
 class Controller:
@@ -15,24 +18,29 @@ class Controller:
         self.njobs = njobs
         self.consumers = consumers
 
-    def run(self):
+    async def run(self):
         """
         Maintains the benchmarking process and blocks until benchmarking is finished (i.e., all jobs are completed).
+        Also blocks until all consumers are finished.
         """
+        tasks = set()
         # submit njobs to the runner
         for _ in range(self.njobs):
             job = self.benchmarker.next_job()
             if job is not None:
                 self.runner.submit(job)
         # iterate over the results
-        for result in self.runner.completions():
+        async for result in self.runner.completions():
             self.benchmarker.handle_result(result)
             # submit the next job
             job = self.benchmarker.next_job()
             if job is not None:
                 self.runner.submit(job)
-            self.__process_result__(result)
+            for consumer in self.consumers:
+                task = asyncio.create_task(consumer.consume_result(result))
+                tasks.add(task)
 
-    def __process_result__(self, result: Result):
-        for consumer in self.consumers:
-            consumer.consume_result(result)
+                task.add_done_callback(tasks.discard)
+
+        if tasks:
+            await asyncio.gather(*tasks)
